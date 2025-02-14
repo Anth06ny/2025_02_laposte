8e05664d2ef40553988e67ce2e5724b3ba8b179e
Anthony <anth06ny@gmail.com>
2025-02-14 14:07:39 +0100

    TP Avant gestion état

--- app/src/main/java/com/amonteiro/a2025_02_laposte/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a2025_02_laposte/ui/screens/SearchScreen.kt
@@ -2,6 +2,7 @@ package com.amonteiro.a2025_02_laposte.ui.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,13 +12,26 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,7 +45,7 @@ import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
 
 @Preview(showBackground = true, showSystemUi = true)
-@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES, locale = "fr")
 @Composable
 fun SearchScreenPreview() {
     //Il faut remplacer NomVotreAppliTheme par le thème de votre application
@@ -46,19 +60,86 @@ fun SearchScreenPreview() {
 @Composable
 fun SearchScreen(modifier: Modifier = Modifier, mainViewModel: MainViewModel = MainViewModel()) {
 
-    Column (modifier= modifier.fillMaxSize()) {
+    Column (modifier= modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+
+        SearchBar()
 
         val list = mainViewModel.dataList.collectAsStateWithLifecycle().value
-        repeat(list.size) {
-            PictureRowItem(data = list[it])
+
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.weight(156f)) {
+            items(list.size) {
+                PictureRowItem(data = list[it])
+            }
+        }
+
+        Row {
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Clear,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text(stringResource(R.string.clear_data))
+            }
+
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Filled.Send,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text(stringResource(R.string.load_data))
+            }
         }
     }
+}
+
+@Composable
+fun SearchBar(modifier : Modifier = Modifier){
+    var search = ""
+    TextField(
+        value = "", //Valeur affichée
+        onValueChange = {newValue:String -> }, //Nouveau texte entrée
+        leadingIcon = { //Image d'icone
+            Icon(
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null
+            )
+        },
+        singleLine = true,
+        label = { //Texte d'aide qui se déplace
+            Text("Enter text")
+            //Pour aller le chercher dans string.xml
+            //Text(stringResource(R.string.placeholder_search))
+        },
+        //placeholder = { //Texte d'aide qui disparait
+        //Text("Recherche")
+        //},
+
+        //keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search), // Définir le bouton "Entrée" comme action de recherche
+        //keyboardActions = KeyboardActions(onSearch = {onSearchAction()}), // Déclenche l'action définie
+        //Comment le composant doit se placer
+        modifier = modifier
+            .fillMaxWidth() // Prend toute la largeur
+            .heightIn(min = 56.dp) //Hauteur minimum
+    )
 }
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable //Composable affichant 1 PictureBean
 fun PictureRowItem(modifier: Modifier = Modifier, data: PictureBean) {
-    Row (modifier = modifier.background(MaterialTheme.colorScheme.onTertiary).fillMaxWidth()) {
+    Row (modifier = modifier
+        .background(MaterialTheme.colorScheme.onTertiary)
+        .fillMaxWidth()) {
 
         //Permission Internet nécessaire
         GlideImage(
@@ -73,8 +154,9 @@ fun PictureRowItem(modifier: Modifier = Modifier, data: PictureBean) {
             failure = placeholder(R.mipmap.ic_launcher),
             contentScale = ContentScale.Fit,
             //même autres champs qu'une Image classique
-            modifier = Modifier.heightIn(max = 100.dp) //Sans hauteur il prendra tous l'écran
-                .widthIn(max= 100.dp)
+            modifier = Modifier
+                .heightIn(max = 100.dp) //Sans hauteur il prendra tous l'écran
+                .widthIn(max = 100.dp)
         )
 
         Column {
